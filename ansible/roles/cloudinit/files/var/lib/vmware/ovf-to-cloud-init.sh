235eea3e98cc5d2d852d1be1bee055e8719b6edd
akutz <akutz@vmware.com>
2020-08-07 17:49:50 -0500

    [haproxy-ova] Update the OVF-to-CI program
    This patch updates the OVF to cloud-init program based on feedback
    regarding the names of the bindServiceToIP functions.

--- ansible/roles/cloudinit/files/var/lib/vmware/ovf-to-cloud-init.sh
+++ ansible/roles/cloudinit/files/var/lib/vmware/ovf-to-cloud-init.sh
@@ -81,14 +81,14 @@ getPermitRootLogin () {
     fi
 }
 
-bindSSHToManagementIP() {
+bindSSHToIP() {
     sed -i -e 's/#ListenAddress 0.0.0.0/ListenAddress '"${1}"'/' /etc/ssh/sshd_config
-    echo "SSH is now bound to the management IP address ${1}"
+    echo "SSH is now bound to IP address ${1}"
 }
 
-bindDataPlaneAPIToManagementIP() {
+bindDataPlaneAPIToIP() {
     sed -i -e 's/--tls-host=0.0.0.0/--tls-host='"${1}"'/' /etc/haproxy/haproxy.cfg
-    echo "Data Plane API is now bound to the management IP address ${1}"
+    echo "Data Plane API is now bound to IP address ${1}"
 }
 
 bindServicesToManagementIP() {
@@ -99,8 +99,8 @@ bindServicesToManagementIP() {
     else
         ip="${ip%/*}"
         echo "binding SSH and Data Plane API to the management IP address ${ip}"
-        bindSSHToManagementIP "${ip}"
-        bindDataPlaneAPIToManagementIP "${ip}"
+        bindSSHToIP "${ip}"
+        bindDataPlaneAPIToIP "${ip}"
     fi
 }
 
